4e36ae940fa9d8fd3515bb3b034057161dc7da41
izume01 <shrey16211@gmail.com>
2024-12-01 01:51:01 +0530

    Implement vault container fetching from the database; update styles and remove hardcoded vaults

--- public/js/getVaultContainer.js
+++ public/js/getVaultContainer.js
@@ -1,2 +1,40 @@
-// This file will be used to get all the vault containers from the database and add them to the DOM
-// IT WILL BE USING THE FETCH API TO ADD ALL THE VAULT CONTAINERS TO THE DOM FROM THE DATABASE
+const vaultContainer = document.querySelector('.vaults');
+
+// Function to get all the vault containers from the database
+const getVaults = async () => {
+    try {
+        const res = await fetch('/api/container/getVaultContainers');
+        if (!res.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await res.json();
+        console.log(data);
+        if (data.Vaults.length === 0) {
+            vaultContainer.innerHTML = '<h2>No Vaults Found</h2>';
+        } else {
+            data.Vaults.forEach(vault => {
+                const dateObj = new Date(vault.createdAt);
+                const date = dateObj.toISOString().split('T')[0]; 
+                const time = dateObj.toTimeString().split(' ')[0]; // '19:57:38'
+
+                const html = `
+                    <div class="vault">
+                        <h3>${vault.title}</h3>
+                        <p>${vault.description}</p>
+                        <p>Date : ${date}</p>
+                        <p>Time: ${time}</p>
+                        <button>Open</button>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                `;
+                vaultContainer.insertAdjacentHTML('beforeend', html);
+            });
+        }
+    } catch (error) {
+        console.error('Get vaults error:', error);
+        vaultContainer.innerHTML = '<h2>Error loading vaults</h2>';
+    }
+}
+
+getVaults();
